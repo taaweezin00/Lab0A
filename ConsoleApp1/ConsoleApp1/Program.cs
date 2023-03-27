bc10cb2481173e791d1b199d328d7cc336b4a35e
taaweezin00 <119288371+taaweezin00@users.noreply.github.com>
2023-03-27 22:13:27 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name ทวีศิลป์: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Lastname ใจดี: ");
+            string lastName = Console.ReadLine();
+            Console.Write("รหัสนักศึกษา 653450509-5: ");
+            string studentId = Console.ReadLine();
+            Console.Write("Email Thaweesin.j@kkumail.com: ");
+            string email = Console.ReadLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
